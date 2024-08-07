de807b8672689df7832b2dc82fe039d2bee30a87
Haoyu Qiu <timothyqiu32@gmail.com>
2024-08-07 14:13:15 +0800

    nvim: add key mapping for luasnip

--- nvim/lua/timothyqiu/plugins/lsp.lua
+++ nvim/lua/timothyqiu/plugins/lsp.lua
@@ -46,6 +46,7 @@ return {
             })
 
             local cmp = require("cmp");
+            local luasnip = require("luasnip")
             require('luasnip.loaders.from_vscode').lazy_load()
             cmp.setup({
                 sources = {
@@ -57,6 +58,24 @@ return {
                 },
                 mapping = cmp.mapping.preset.insert({
                     ['<CR>'] = cmp.mapping.confirm({ select = false }),
+                    ['<Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.locally_jumpable(1) then
+                            luasnip.jump(1)
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                    ['<S-Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
                 }),
                 preselect = "item",
                 completion = {
